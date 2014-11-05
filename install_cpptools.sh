93b5e71be2d21fd5ac3efb083710cfe41f9152be
alexyun <alexyun@castis.com>
2014-11-05 14:15:53 +0900

    cpplint.py 설치 후 삭제

--- install_cpptools.sh
+++ install_cpptools.sh
@@ -9,4 +9,5 @@ wget http://google-styleguide.googlecode.com/svn/trunk/cpplint/cpplint.py
 sed -e s/'\/usr\/bin\/python'/'\/usr\/local\/bin\/python2.7'/g cpplint.py > tmp_cpplint
 mv tmp_cpplint cpplint.py
 install cpplint.py /usr/local/bin/cpplint
+rm -rf cpplint.py
 
