524dd72b9d56d47926068e20f55f41cc535fc193
alexyun <pinkcubrick@gmail.com>
2015-05-27 10:17:54 +0900

    docker hub에서의 g++ internal compiler error를 막기 위해

--- install_boost157.sh
+++ install_boost157.sh
@@ -5,8 +5,8 @@ wget http://sourceforge.net/projects/boost/files/boost/1.57.0/boost_1_57_0.tar.g
 tar xzf boost_1_57_0.tar.gz
 cd boost_1_57_0
 ./bootstrap.sh
-./b2 --prefix=/usr/local/boost157 --without-python -j4 install
-./b2 boost.locale.icu=off --with-locale --prefix=/usr/local/boost157 -j4 install
+./b2 --prefix=/usr/local/boost157 --without-python install
+./b2 boost.locale.icu=off --with-locale --prefix=/usr/local/boost157 install
 cd ~
 rm -rf boost_1_57_0*
 
