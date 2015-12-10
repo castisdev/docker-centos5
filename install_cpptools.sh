35781e085f49134067107ad31a22df6957c68728
sasgas <sasgas@castis.com>
2015-12-10 14:44:55 +0900

    mysql-devel postgres-devel 추가

--- install_cpptools.sh
+++ install_cpptools.sh
@@ -3,10 +3,9 @@
 wget https://bootstrap.pypa.io/get-pip.py --no-check-certificate
 python2.7 get-pip.py
 pip install gcovr
-rm -rf get-pip.py 
+rm -rf get-pip.py
 
-wget http://google-styleguide.googlecode.com/svn/trunk/cpplint/cpplint.py
-sed -i -e s/'\/usr\/bin\/python'/'\/usr\/local\/bin\/python2.7'/g cpplint.py
+wget https://github.com/google/styleguide/raw/gh-pages/cpplint/cpplint.py --no-check-certificate
+sed -i -e s/'\/usr\/bin\/env python'/'\/usr\/bin\/env python2.7'/g cpplint.py
 install cpplint.py /usr/local/bin/cpplint
 rm -rf cpplint.py
-
