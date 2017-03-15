b3ab6a3e07cfc18693359e22d55cfbfed494c8cb
sasgas <sasgas@castis.com>
2017-03-15 11:23:12 +0900

    minor fix

--- install_devtoolset2.sh
+++ install_devtoolset2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 cd /etc/yum.repos.d
 wget http://people.centos.org/tru/devtools-2/devtools-2.repo
-yum install -y devtoolset-2-gcc-c++ devtoolset-2-binutils devtoolset-2-libquadmath-devel; yum clean all -y
+yum install -y devtoolset-2-gcc-c++ devtoolset-2-binutils devtoolset-2-libquadmath-devel devtoolset-2-gdb devtoolset-2-gdb-server; yum clean all -y
