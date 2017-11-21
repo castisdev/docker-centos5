7a5b5924a01be411d24016c33aa342ddac63ebe1
sasgas <sasgas@castis.com>
2017-11-21 15:40:22 +0900

    libasan-devel 추가

--- install_devtoolset2.sh
+++ install_devtoolset2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 cd /etc/yum.repos.d
 wget http://people.centos.org/tru/devtools-2/devtools-2.repo
-yum install -y devtoolset-2-gcc-c++ devtoolset-2-binutils devtoolset-2-libquadmath-devel devtoolset-2-gdb devtoolset-2-gdb-server devtoolset-2-perftools; yum clean all -y
+yum install -y devtoolset-2-gcc-c++ devtoolset-2-binutils devtoolset-2-libquadmath-devel devtoolset-2-gdb devtoolset-2-gdb-server devtoolset-2-perftools devtoolset-2-libasan-devel; yum clean all -y
